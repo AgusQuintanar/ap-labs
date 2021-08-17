8447db3e0c35a81889e33c2a6cd18ac34d5e8d1f
Katacoda Scenario <scenario@katacoda.com>
2021-08-17 21:30:39 +0000

    modifies 'test-lab.go' in order to support a personalized hello message

--- labs/test-lab/test-lab.go
+++ labs/test-lab/test-lab.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the jungle")
+  user_args := os.Args[1:]
+  user := strings.Join(user_args ," ")
+
+  
+  if len(user) == 0 {
+    	fmt.Println("Error: You didn't provide a valid user name.")
+    	fmt.Println("Usage: 'go run test-lab.go <user name>'")
+    	return
+  }
+  
+  
+	fmt.Println("Hello " + user + ", Welcome to the Jungle")
+	
 }
