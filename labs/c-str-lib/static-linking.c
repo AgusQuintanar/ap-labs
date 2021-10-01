f45f767123572e76d279c7bac31add58803c8f91
AgusQuintanar <agusquintanar17@gmail.com>
2021-09-30 19:22:04 -0500

    c-str-lib lab

--- labs/c-str-lib/static-linking.c
+++ labs/c-str-lib/static-linking.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+extern int mystrlen(char *);
+extern char *mystradd(char *, char *);
+extern int mystrfind(char *, char *);
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("ERROR: missing arguments or bad inputs\n");
+        printf("Example usage: $ ./main.o -add \"Initial String \" \"This is the rest to be added\"\n");
+        return -1;
+    }
+
+    if (strcmp(argv[1], "-add") == 0) {
+        printf("Initial Lenght      : %d\n", mystrlen(argv[2]));
+        char *new_string = mystradd(argv[2],argv[3]);
+        printf("New String          : %s\n", new_string);
+        printf("New length          : %d\n", mystrlen(new_string));
+    }
+
+    if (strcmp(argv[1], "-find") == 0) {
+        int find = mystrfind(argv[2], argv[3]);
+        if (find != -1) {
+            printf("['%s'] string was found at [%d] position\n", argv[3], find);
+        } 
+        else {
+            printf("No ocurrences were found");
+        }
+    }
     return 0;
 }
--- labs/c-str-lib/strlib.c
+++ labs/c-str-lib/strlib.c
@@ -1,13 +1,42 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int len = 0;
+    while (*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    char *origin_end = origin + mystrlen(origin); // goes to the end of the origin str
+
+    while (*addition != '\0') {
+        *origin_end++ = *addition++;
+    }
+
+    *origin_end = '\0';
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int pOrigin = 0,
+        pSubstr = 0,
+        objective = mystrlen(substr),
+        curr = 0;
+
+    while (origin[pOrigin] != '\0') {
+        curr = pOrigin;
+        while (origin[pOrigin] == substr[pSubstr] && origin[pOrigin] != '\0') {
+            pOrigin++;
+            pSubstr++;
+        }
+        if (pSubstr == objective) {
+            return curr;
+        }
+        pOrigin++;
+        pSubstr = 0;
+    }
+    return -1;
 }
